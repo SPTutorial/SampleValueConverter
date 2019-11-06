55df25a5cde9ebdf737c64162b0825ba0bd335f4
Subrata Pal <sub.pal28@gmail.com>
2019-11-06 21:16:13 +0530

    Add project files.

---
+++ SampleValueConverter/SampleValueConverter/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SampleValueConverter
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            List<Data> myData = new List<Data>();
+            myData.Add(new Data { Name = "This is red color", ColorNumber = 1 });
+            myData.Add(new Data { Name = "This is blue color", ColorNumber = 4 });
+            myData.Add(new Data { Name = "This is green color", ColorNumber = 2 });
+            myData.Add(new Data { Name = "This is violate color", ColorNumber = 5 });
+            myData.Add(new Data { Name = "This is orange color ", ColorNumber = 3 });
+
+            listData.ItemsSource = myData;
+        }
+    }
+    public class Data
+    {
+        public string Name { get; set; }
+        public int ColorNumber { get; set; }
+    }
+}
---
+++ SampleValueConverter/SampleValueConverter/NumberToColorConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SampleValueConverter
+{
+    public class NumberToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string color = "";
+            int colorNumber = (int)value;
+            switch (colorNumber)
+            {
+                case 1:
+                    color = "#ed0707";  // red
+                    break;
+                case 2:
+                    color = "#34db25"; // green
+                    break;
+                case 3:
+                    color = "#fcd46d"; // orange
+                    break;
+                case 4:
+                    color = "#2e15eb"; // blue
+                    break;
+                case 5:
+                    color = "#0ff2e3"; //violate
+                    break;
+                default:
+                    color = "#000000";
+                    break;
+            }
+            return color;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
